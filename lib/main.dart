79dbb04261cb8c645458b9462770070c8f11971a
Jake Glick <jake.glick@student.moriah.nsw.edu.au>
2024-02-24 21:35:24 +1100

    extract quoteTemplate into QuoteCard widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -23,31 +24,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote(text: 'Hard work beats talent', author: 'Mike Tyson'),
   ];
 
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            Text(
-              quote.author,
-              style: TextStyle(fontSize: 14, color: Colors.grey[800]),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,7 +36,7 @@ class _QuoteListState extends State<QuoteList> {
           backgroundColor: Colors.redAccent,
         ),
         body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+          children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ));
   }
 }
